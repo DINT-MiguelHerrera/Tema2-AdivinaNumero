f781f73ee2fcd300f99c79860a639df77741186b
alumno <alumno@A302PC35>
2023-10-04 09:29:05 +0200

    AdivinaNumero Terminado

--- Tema2-AdivinaNumero/MainWindow.xaml.cs
+++ Tema2-AdivinaNumero/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,54 @@ namespace Tema2_AdivinaNumero
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int numAleatorio;
+
         public MainWindow()
         {
             InitializeComponent();
+            Random random = new Random();
+            numAleatorio = random.Next(0, 101);
+
+        }
+
+        private void BotonComprobar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int numero = int.Parse(CasillaIntroduceNumero.Text);
+
+                if (numero < numAleatorio)
+                {
+                    CasillaInforma.Text = "Te has quedado corto";
+                    CasillaInforma.Foreground = Brushes.Red;
+                }
+                else if (numero > numAleatorio)
+                {
+                    CasillaInforma.Text = "Te has pasado";
+                    CasillaInforma.Foreground = Brushes.Red;
+                }
+                else if (numero == numAleatorio)
+                {
+                    CasillaInforma.Text = "Lo has adivinado";
+                    CasillaInforma.Foreground = Brushes.Green;
+                    e.Handled = true;
+                }
+            }
+            catch(Exception ex)
+            {
+                CasillaInforma.Text = "Escriba un número";
+                CasillaInforma.Foreground = Brushes.Black;    
+            }
+            
+
+        }
+
+        private void BotonReiniciar_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            numAleatorio = random.Next(0, 101);
+            CasillaIntroduceNumero.Text = "";
+            CasillaInforma.Text = "";
         }
     }
 }
